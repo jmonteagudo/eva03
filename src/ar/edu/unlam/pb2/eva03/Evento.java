0f09b2cb1bf8215caa5aaf42e0d8364169930562
Juan <jmonteagudo@gmail.com>
2020-11-25 08:14:11 -0300

    Cambios de ultimo momento.

--- src/ar/edu/unlam/pb2/eva03/Evento.java
+++ src/ar/edu/unlam/pb2/eva03/Evento.java
@@ -13,18 +13,4 @@ public class Evento {
 	private Integer numeroDeInscripcion;
 	private Map<Integer, Deportista> participantes;
 	
-	public Evento(TipoDeEvento tipo) {
-		this.tipo = tipo;
-		this.numeroDeInscripcion = 0;
-		participantes = new HashMap<Integer, Deportista>();
-	}
-	
-	public Integer agregarParticipante(Deportista nuevo) {
-		participantes.put(++this.numeroDeInscripcion, nuevo);
-		return this.numeroDeInscripcion;
-	}
-	
-	public TipoDeEvento getTipo() {
-		return tipo;
-	}
 }
